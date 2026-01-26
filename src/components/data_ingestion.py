5bbc6378d0e081061896eed7f92e149e10e54407
Disha Sharma <sharmadisha1629@gmail.com>
2026-01-26 17:30:42 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -18,9 +20,19 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Enter the data ingestion")
         try:
-            df=pd.read_csv(r'notebook\data\StudentsPerformance.csv')
+            current_dir = os.path.dirname(__file__)
+
+            file_path = os.path.join(
+                current_dir,
+                "..", "..",
+                "notebook", "data", "StudentsPerformance.csv"
+            )
+            file_path = os.path.abspath(file_path) 
+
+            df=pd.read_csv(file_path)
             logging.info("read the datasets")
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
@@ -37,7 +49,9 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
     
     
     
