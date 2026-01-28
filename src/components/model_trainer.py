2ef54b9a23f71a1e534e1e82de94fad0821ed1ba
Disha Sharma <sharmadisha1629@gmail.com>
2026-01-28 19:48:36 +0530

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -15,7 +15,56 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 
 @dataclass
-class ModelTrainer
+class ModelTrainerConfig:
+    trainer_model_file_path=os.path.join("artifact","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+    def initiate_model_trainer(self,train_arr,test_arr):
+        try:
+            logging.info("split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+            models={
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree":DecisionTreeRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
+                "Linear Regression":LinearRegression(),
+                "k-neighbors Ckassifier":KNeighborsRegressor(),
+                "XGBClassifier":XGBRegressor(),
+                "CatBoostClassifier":CatBoostRegressor(verbose=False),
+                "AdaBoost Classifier":AdaBoostRegressor(),
+            }
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                              models=models)
+            # to get best model score from dict
+            best_model_score=max(sorted(model_report.values()))
+
+            # to get best model name from dict
+
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+
+            ]
+            best_model=models[best_model_name]
+            if best_model_score<0.6:
+                raise CustomException("No brst model found")
+            logging.info(f"Best found model on both training and testing datasets")
+            save_object(
+                file_path=self.model_trainer_config.trainer_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(X_test)
+            r2=r2_score(y_test,predicted)
+            return r2
+        except Exception as e:
+            raise CustomException(e,sys)
+            
